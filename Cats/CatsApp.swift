d62f29e1311587446123b3e29bde274667e60b62
Sebastian Dąbrowki <sebastian.dabrowski@redge.com>
2024-05-16 12:07:00 +0200

    Initial Commit

---
+++ Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by Sebastian Dąbrowski on 16.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
